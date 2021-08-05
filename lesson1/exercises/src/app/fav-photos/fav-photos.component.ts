b35557c0c95c04c9f2a401a088c2ed5a61eb07c7
Hope Ciuffa <hopeciuffa@gmail.com>
2021-08-05 15:16:50 -0500

    part 1 updates

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2VhJTIwYmVhY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80';
+  image3 = 'https://render.fineartamerica.com/images/rendered/default/print/5.5/8/break/images-medium-5/jamaica-hammock-on-beach-at-sunset-tetra-images.jpg';
 
   constructor() { }
 
